6dc45618e8a8a9e3d6aa398479d2349cfb1ce522
Ryan Bowlen <rabowlen@gmail.com>
2018-04-29 17:48:14 -0500

    Update media.py
    Updated docstring to better explain the class, its contents, and how to use it.

--- media.py
+++ media.py
@@ -2,7 +2,26 @@ import webbrowser
 
 
 class Movie():
-    """This class stores and displays movie-related information"""
+    """This class stores and displays movie-related information
+
+    Note:
+        Do not include the `self` parameter in the ``Args`` section.
+
+    Args:
+        movie_title (str): The movie's full title.
+        movie_storyline (str): A short description of the plot.
+        poster_image (str): A url for the movie's poster.
+        trailer_youtube (str): A url for the movie's trailer.
+
+    Attributes:
+        title (str): The movie's full title.
+        storyline (str): A short description of the plot.
+        poster_image_url (str): A url for the movie's poster.
+        trailer_youtube_url (str): A url for the movie's trailer.
+
+    Methods:
+        show_trailer(): shows user a trailer for the movie on image click.
+    """
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
     def __init__(self, movie_title, movie_storyline, poster_image,
